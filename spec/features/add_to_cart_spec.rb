424ab559759ee65c5b859926a52436772d1e946c
Alex-Reyne <creativereyne@gmail.com>
2022-01-11 19:57:04 -0600

    create testing-add-to-cart spec

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,13 +16,14 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
     end
   end
 
-  scenario "They navigate to the product details page" do
+  scenario "They add an item to their cart" do
     visit root_path
-    first('.btn-default').click
-    
-    expect(page).to have_link 'Apparel', href: '/categories/1'
+
+    first('.btn-primary').click
+
+    expect(page).to have_text 'My Cart (1)'
     
     # commented out b/c it's for debugging only
-    # save_and_open_screenshot
+    save_and_open_screenshot
   end
 end
